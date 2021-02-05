39a47c9cb382f32417dc43f15af04b1b21d5fdbe
Rafael Couto <rafaelcsilva016@outlook.com>
2021-02-05 15:22:35 -0300

    update styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import App from './pages/app';
 
+
 render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.querySelector('#root')
 );
--- src/pages/players/styles.ts
+++ src/pages/players/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/macro';
 
+
 export const Main = styled.main`
     width: 100vw;
     height: 100vh;
@@ -14,17 +15,37 @@ export const BoxData = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    width: 80%;
+
+    @media screen and (min-width: 400px){
+        width: 100%;
+    }
 `;
 
 export const Label = styled.label`
     margin: 20px 0;
+    font-size: 16px;
+
+    @media screen and (min-width: 400px){
+        font-size: 20px;
+    }
 `;
 
 export const Input = styled.input`
     padding: 5px;
+    font-size: 14px;
+
+    @media screen and (min-width: 400px){
+        font-size: 16px;
+    }
 `;
 
 export const Select = styled.select`
     padding: 4px;
     margin: 0 0 0 10px;
+    font-size: 14px;
+
+    @media screen and (min-width: 400px){
+        font-size: 16px;
+    }
 `;
