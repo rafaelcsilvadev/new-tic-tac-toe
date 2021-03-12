5c5f3dd3c66ce4f2c363d34ab5c4f4c3cba1cc6e
Rafael Couto <rafaelcsilva016@outlook.com>
2021-03-12 19:10:44 -0300

    Update index.tsx

--- src/pages/inputData/index.tsx
+++ src/pages/inputData/index.tsx
@@ -55,7 +55,8 @@ const InputData: React.FC = ({ playersDispatch }: any) => {
               name="symbol"
               id="symbol"
               value={symbol}
-              onChange={(e) => setSymbol(e.target.value)}>
+              onChange={(e) => setSymbol(e.target.value)}
+            >
               <option defaultChecked value="true">
                 X
               </option>
